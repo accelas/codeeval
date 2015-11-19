b57689744ba17814f48a59817f0570554dc5d0cd
Kai Wang <kai@foldr.ca>
2015-11-19 14:47:37 -0800

    adding hidden digits

---
+++ 122/hidden_digits.py
@@ -0,0 +1,39 @@
+import sys
+
+test_cases = open(sys.argv[1], 'r')
+
+chars = {
+    'a' : '0',
+    'b' : '1',
+    'c' : '2',
+    'd' : '3',
+    'e' : '4',
+    'f' : '5',
+    'g' : '6',
+    'h' : '7',
+    'i' : '8',
+    'j' : '9',
+             
+    '0' : '0',
+    '1' : '1',
+    '2' : '2',
+    '3' : '3',
+    '4' : '4',
+    '5' : '5',
+    '6' : '6',
+    '7' : '7',
+    '8' : '8',
+    '9' : '9',
+}
+
+for test in test_cases:
+    res = []
+    for c in test:
+        try:
+            res.append(chars[c])
+        except:
+            pass
+    if not res:
+        print("NONE")
+    else:
+        print(''.join(res))

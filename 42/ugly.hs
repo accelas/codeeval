3a513b4d594c0d1dd62627331c571a217cbfcbb5
Kai Wang <kai@foldr.ca>
2015-11-20 16:40:12 -0800

    optimize memory usage

--- 42/ugly.hs
+++ 42/ugly.hs
@@ -1,15 +1,22 @@
-
 import System.Environment (getArgs)
 import Control.Monad
 import Control.Applicative
-import Data.List
+import Data.Traversable
+import qualified Data.Vector as V
 
+data Op = ADD | SUB | NON -- deriving (Eq, Ord, Enum, Read, Show)
 
-is_ugly num = elem 0 $ mod (abs num) <$> [2,3,5,7]
+data S = S {-# UNPACK #-} !Int {-# UNPACK #-} !Int
+data I = I {-# UNPACK #-} !Int {-# UNPACK #-} !Op
 
-data Op = ADD | SUB | NON deriving (Eq, Ord, Enum, Read, Show)
+isUgly num
+    | mod num 2 == 0 = True
+    | mod num 3 == 0 = True
+    | mod num 5 == 0 = True
+    | mod num 7 == 0 = True
+    | otherwise = False
 
-opsGen num = replicateM num [ADD, SUB, NON]
+opsGen num = V.replicateM num [ADD, SUB, NON]
 
 char2Int '1' = 1
 char2Int '2' = 2
@@ -22,34 +29,27 @@ char2Int '8' = 8
 char2Int '9' = 9
 char2Int _   = 0
 
-_calc :: (Int, Int) -> (Int, Op) -> ((Int, Int), Int)
-_calc (sign, carry) (dig0, ADD) = ((1, 0), carry + sign * dig0)
-_calc (sign, carry) (dig0, SUB) = ((-1, 0), carry + sign * dig0)
-_calc (sign, carry) (dig0, NON) = ((sign, (carry + sign * dig0) * 10), 0)
-
-calc ops num = sign * (last num) + carry + sum res
+calc :: V.Vector Op -> V.Vector Int -> Int
+calc ops num = sign * V.last num + carry + V.sum res
   where
-    ((sign, carry), res) = mapAccumL _calc (1,0) (zip (init num) ops)
+    _calc :: S -> I -> (S, Int)
+    _calc (S sign carry) (I dig0 ADD) = (S   1  0, carry + sign * dig0)
+    _calc (S sign carry) (I dig0 SUB) = (S (-1) 0, carry + sign * dig0)
+    _calc (S sign carry) (I dig0 NON) = (S sign ((carry + sign * dig0) * 10), 0)
 
-proc num = [calc o (char2Int <$> num) | o <- opsGen (length num - 1)]
+    (S sign carry, res) = mapAccumL _calc (S 1 0) (V.zipWith I (V.init num) ops)
 
-result n = length $ (filter is_ugly) (proc n)
+result :: String -> String
+result num =
+    let _num = V.fromList $ char2Int <$> num
+        _len = length num - 1
+    in show . length $ do
+        o <- opsGen _len
+        void $ guard (isUgly . abs $ calc o _num)
 
 main = do
     [inpFile] <- getArgs
     input <- readFile inpFile
-    mapM_ putStrLn $ show . result <$> lines input
+    let res = result <$> lines input
+    mapM_ putStrLn res
 
-_proc num = [(debug_calc o (char2Int <$> num)) | o <- opsGen (length num - 1)]
-
-debug_calc ops num = (p_input l t) ++ (show $ calc ops num)
-  where
-    l = (zip (init num) ops)
-    t = last num
-
-p_input ::[(Int, Op)] -> Int -> String
-p_input l t = concat $ [ show d ++ _show o | (d , o) <- l ] ++ [show t ++ " == "]
-  where
-    _show NON = ""
-    _show ADD = " + "
-    _show SUB = " - "
